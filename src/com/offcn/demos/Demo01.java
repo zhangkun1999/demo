6a50c6d57d789d0d94b8fd94b6479c166359b73a
zhangkun1999 <106510529+zhangkun1999@users.noreply.github.com>
2022-05-31 21:30:34 +0800

    Update Demo01.java

--- src/com/offcn/demos/Demo01.java
+++ src/com/offcn/demos/Demo01.java
@@ -4,5 +4,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("idea中集成git");
          System.out.println("这是给pull的");
+        System.out.println("这是我在github上添加的");
     }
 }
