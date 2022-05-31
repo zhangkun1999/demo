dfbc4d0f3ed40c0fed6de2b0bb97e55c7ab8536d
ZK <zhangkun199999@163.com>
2022-05-31 21:29:01 +0800

    分支合并后的修改

--- src/com/offcn/demos/Demo01.java
+++ src/com/offcn/demos/Demo01.java
@@ -4,5 +4,7 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("idea中集成git");
          System.out.println("这是给pull的");
+        System.out.println("这是分支dev添加的");
+        System.out.println("jsjs");
     }
 }
